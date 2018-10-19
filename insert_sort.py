74ad72201a52b42e97c751e748e362a3a0516ea6
aentisom <43248520+aentisom@users.noreply.github.com>
2018-10-20 01:01:11 +0800

    Update insert_sort.py

--- insert_sort.py
+++ insert_sort.py
@@ -1,21 +1,18 @@
 def myInsertSort(theList):
-    
     newList = []
     newList.append(theList[0])
     
     for i in theList[1:]:
-
         if i >= newList[-1]:
             newList.append(i)
         elif i <= newList[0]:
             newList.insert(0,i)
         else:
-            l = len(newList)
+            L = len(newList)
             
-            for j in range(l):
-                
-                if newList[j] <= i and i <= newList[j + 1]:
-                    newList.insert(j + 1,i)
+            for flag in range(L):
+                if newList[flag] <= i and i <= newList[flag + 1]:
+                    newList.insert(flag + 1,i)
                     break
 
     return newList
