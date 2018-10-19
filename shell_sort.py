a39863a5ba74f28d3ad5ff222ec8954bb9a77872
aentisom <43248520+aentisom@users.noreply.github.com>
2018-10-19 23:05:39 +0800

    Update shell_sort.py

---
+++ shell_sort.py
@@ -0,0 +1,62 @@
+'''
+def findMin(theList):#求最小数
+    minNum = theList[0]
+    flag = 0
+    for i in range(len(theList)):    
+        if minNum > theList[i]:
+            minNum = theList[i]
+            flag = i
+    return [minNum,flag]
+
+def ergodic(theList):#遍历排序(低效,已废弃)
+    newList = []
+    while len(theList) != 0:
+        theMin = findMin(theList)
+        newList.append (theMin[0])
+        del theList[theMin[1]] 
+    return newList
+'''
+
+import insert_sort
+
+
+def shellList(the_list):#希尔增量
+    shell_list = []
+    N = len(the_list)
+    
+    while N != 1:
+        N = int(N/2)
+        shell_list.append(N)
+        
+    return shell_list
+
+def myShell(the_list):#希尔排序
+    
+    gap_list=shellList(the_list)
+    L=len(the_list)
+    
+    for gap in gap_list:
+
+        if gap == 1:
+            return insert_sort.myInsertSort(the_list)
+        
+        flag = 0#flag是分组的序号
+        
+        for i in range(gap):
+            list_temp = []
+            
+            while flag < L:#提取分组
+                list_temp.append(the_list[flag])
+                flag += gap
+                
+            insert_list = insert_sort.myInsertSort(list_temp)#inser_list变量,insert_sort模块,排序分组
+            k = -1
+            flag -= gap
+            
+            while flag >= 0:#使用新分组替换原列表对应元素
+                the_list[flag] = insert_list[k]#k是insert_list专用索引
+                k -= 1
+                flag -= gap
+                
+            flag = i + 1
+
